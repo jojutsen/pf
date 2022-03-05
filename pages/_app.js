62b511a762de15bd60ceae3972cb7390488539d9
mbaharip <main@mbaharip.me>
2022-03-05 15:04:05 +0700

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<NextSeo
+				title='mbahArip Homepage'
+				description='Arief Rachmawan personal page.'
+				canonical='https://www.mbaharip.me/'
+				openGraph={{
+					url: 'https://www.mbaharip.me/',
+					title: 'mbahArip Homepage',
+					description: 'Arief Rachmawan personal page.',
+					images: [
+						{
+							url: 'https://www.mbaharip.me/og-img.png',
+						},
+					],
+					site_name: 'mbahArip Homepage',
+				}}
+				twitter={{
+					handle: '@mbaharip_',
+					site: '@mbaharip_',
+					cardType: 'summary_large_image',
+				}}
+			/>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
