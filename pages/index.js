ee5e9b12c61631d05fa5c746f2485b100ef2410b
mbaharip <main@mbaharip.me>
2022-04-17 12:23:53 +0700

    Add: under construction image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { isMobile } from 'react-device-detect';
 import { IoLogoFacebook, IoLogoTwitter, IoLogoGithub, IoLogoLinkedin, IoMdClose } from 'react-icons/io';
 import { FaArtstation } from 'react-icons/fa';
 import { GiHamburgerMenu } from 'react-icons/gi';
@@ -35,67 +34,50 @@ const social = [
 ];
 
 export default function Home() {
-	const [imgSize, setImgSize] = useState({ width: 512, height: 512 });
+	const [imgSize, setImgSize] = useState({ width: 512, height: 256 });
 	const [expand, setExpand] = useState(false);
 
-	useEffect(() => {
-		if (isMobile) {
-			setImgSize({ width: 256, height: 256 });
-		}
-	}, []);
-
 	return (
-		<div className='w-screen max-h-screen h-screen bg-zinc-900 overflow-hidden grid place-items-center'>
-			<Image src={`/main.svg`} alt={`mbahArip`} width={imgSize.width} height={imgSize.height} objectFit={`contain`} />
-			{isMobile ? (
-				<>
-					<div>
-						{expand ? (
-							<span
-								className={`text-zinc-400 transition-all duration-150 fixed top-6 right-6 z-50`}
-								onTouchStart={() => {
-									setExpand(false);
-								}}
-							>
-								<IoMdClose size={36} />
-							</span>
-						) : (
-							<span
-								className={`text-zinc-400 transition-all duration-150 fixed top-6 right-6 z-50`}
-								onTouchStart={() => {
-									setExpand(true);
-								}}
-							>
-								<GiHamburgerMenu size={36} />
-							</span>
-						)}
-					</div>
-					<div className={`w-screen h-screen absolute bg-zinc-800 z-10 ${expand ? 'top-0' : '-top-[100vh]'} transition-all duration-150`}>
-						<div className='flex flex-col w-full h-full text-zinc-400 justify-center px-8 gap-8 text-xl'>
-							{social.map((item) => (
-								<Link key={item.name} href={item.url}>
-									<a
-										target={`_blank`}
-										className='flex items-center gap-2 text-zinc-400 hover:text-zinc-200 transition-color duration-150'
-									>
-										{item.icon} {item.name}
-									</a>
-								</Link>
-							))}
-						</div>
-					</div>
-				</>
-			) : (
-				<div className={`w-screen absolute bottom-4 p-4 flex gap-4 overflow-hidden justify-center`}>
+		<div className='bg-zinc-900 w-screen m-auto grid place-items-center'>
+			<div className='w-screen max-w-screen-lg max-h-screen h-screen bg-zinc-900 flex flex-col relative'>
+				<button className='absolute top-0 right-0 text-zinc-50 m-4 p-2 md:hidden z-20' onClick={() => setExpand(!expand)}>
+					{expand ? <IoMdClose size={32} /> : <GiHamburgerMenu size={32} />}
+				</button>
+				<div
+					className={`w-full h-full absolute ${
+						expand ? 'top-0' : '-top-full'
+					} left-0 bg-zinc-900 z-10 transition-all duration-150 flex items-start justify-center gap-4 flex-col`}
+				>
 					{social.map((item) => (
 						<Link key={item.name} href={item.url}>
-							<a target={`_blank`} className='flex items-center gap-1 text-zinc-400 hover:text-zinc-200 transition-color duration-150'>
+							<a
+								target={`_blank`}
+								className='flex items-center gap-1 text-zinc-400 hover:text-zinc-200 transition-color duration-150 mx-8 text-lg'
+							>
 								{item.icon} {item.name}
 							</a>
 						</Link>
 					))}
 				</div>
-			)}
+				<div className='w-2/3 h-full mx-auto flex flex-col items-center justify-center gap-8'>
+					<Image src={`/main.svg`} alt={`mbahArip`} width={imgSize.width} height={imgSize.height} objectFit={`contain`} />
+					<div className={`w-full md:flex gap-4 overflow-hidden justify-center hidden`}>
+						{social.map((item) => (
+							<Link key={item.name} href={item.url}>
+								<a
+									target={`_blank`}
+									className='flex items-center gap-1 text-zinc-400 hover:text-zinc-200 transition-color duration-150'
+								>
+									{item.icon} {item.name}
+								</a>
+							</Link>
+						))}
+					</div>
+				</div>
+				<div className='absolute bottom-0 left-0 p-0 m-0 flex items-end md:w-auto w-2/3'>
+					<Image src={`/under-construction.png`} alt={`Under Construction`} width={1077 / 3} height={658 / 3} objectFit={`contain`} />
+				</div>
+			</div>
 		</div>
 	);
 }
