9c40cc927426cdfc6865ce71dbcadd178197e0a9
mbaharip <main@mbaharip.me>
2022-04-17 12:25:27 +0700

    Update: under construction size

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
 					</div>
 				</div>
 				<div className='absolute bottom-0 left-0 p-0 m-0 flex items-end md:w-auto w-2/3'>
-					<Image src={`/under-construction.png`} alt={`Under Construction`} width={1077 / 3} height={658 / 3} objectFit={`contain`} />
+					<Image src={`/under-construction.png`} alt={`Under Construction`} width={1077 / 2.5} height={658 / 2.5} objectFit={`contain`} />
 				</div>
 			</div>
 		</div>
